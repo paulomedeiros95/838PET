4e4ddaca8a00336a665aa24f7114136fc1dd8d94
Gabriel Gomes <gabrielgoliveira04@outlook.com>
2020-08-16 17:39:40 -0300

    adicionado pagina ADMPRODUCTPANEL

---
+++ 838PET/Models/PetProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _838PET.Models
+{
+    public class PetProduct
+    {
+
+        #region Prop
+
+        [Key]
+        public int ProductID { get; set; }
+
+        public double Valor { get; set; }
+
+        #endregion
+    }
+}
