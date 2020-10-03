60576de8d0801a3fefad5196974c72a545e0e4b4
Paulo Medeiros <paulo_workjobs@outlook.com>
2020-10-03 18:51:30 -0300

    Ajustado OnGet da pagina admProductPanel para buscar  no banco o valor do produto cadastrado.

--- 838PET/Pages/AdmProductPanel.cshtml.cs
+++ 838PET/Pages/AdmProductPanel.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _838PET.Areas.Application.Data;
+using _838PET.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,18 @@ namespace _838PET.Pages
 {
     public class AdmProductPanelModel : PageModel
     {
-        public void OnGet()
-        {
 
+
+        [BindProperty]
+        public string ProductValue { get; set; }
+
+        public async Task OnGet()
+        {
+            using (var context = new ApplicationDBContext())
+            {
+                var x = context.Produtos.FirstOrDefault();
+                ProductValue = x.Valor.ToString();
+            }
         }
     }
 }
